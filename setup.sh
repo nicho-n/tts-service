62253b689cd0e529b18284f55149bb8361ee88ef
nick <nicholaspnowak@gmail.com>
2020-01-20 04:25:53 -0500

    update setup script.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-psql postgres -f db/scripts/create_database.sql  -v v1=$USER
-sudo -u $USER psql -f db/scripts/create_tables.sql tts -v v1=$USER
+psql postgres -f db/scripts/create_database.sql  -v v1=$USER > /dev/null
+sudo -u $USER psql -f db/scripts/create_tables.sql tts -v v1=$USER > /dev/null
 cd db;
 rm -rf config;
 mkdir config;
