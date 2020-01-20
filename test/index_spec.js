6d4ee207b8f9e4efa8233287f77cc408ed15f40e
nicho-n <nicholaspnowak@gmail.com>
2020-01-20 04:41:39 -0500

    add test case for 404 response.

--- test/index_spec.js
+++ test/index_spec.js
@@ -8,12 +8,21 @@ chai.should();
 
 describe("TTSService", () => {
     describe("Get /:brand/:name", () => {
-         it("should get TTS averages", (done) => {
+        it("should get TTS averages", (done) => {
             chai.request(app)
                 .get('/Adidas/Yeezy')
                 .set({'Content-Type': 'application/json'})
                 .end((err, res) => {
                     res.should.have.status(200);
+                    done();
+                 });
+        });
+        it("should get return 404 for nonexistent shoe", (done) => {
+            chai.request(app)
+                .get('/Imaginary/Shoe')
+                .set({'Content-Type': 'application/json'})
+                .end((err, res) => {
+                    res.should.have.status(404);
                     done();
                  });
         });
