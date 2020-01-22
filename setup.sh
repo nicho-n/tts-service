2bff8012aa0ee69374a7474296acb969eb0570c3
nick <nicholaspnowak@gmail.com>
2020-01-21 19:32:14 -0500

    cd into correct directory in setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-psql postgres -f db/scripts/create_database.sql  -v v1=$USER > /dev/null
-psql -f db/scripts/create_tables.sql tts -v v1=$USER > /dev/null
-cd db;
+psql postgres -f service/db/scripts/create_database.sql  -v v1=$USER > /dev/null
+psql -f service/db/scripts/create_tables.sql tts -v v1=$USER > /dev/null
+cd service/db;
 rm -rf config;
 mkdir config;
 echo '
