35642db5d2d4ced63858480e697e9ad70c92415b
nick <nicholaspnowak@gmail.com>
2020-01-21 19:40:31 -0500

    cleanup

--- service/db/queries.js
+++ service/db/queries.js
@@ -10,8 +10,4 @@ const getAverage = (req) => {
     return pool.query(`SELECT ttsData FROM ttsData WHERE brand = $1 AND name = $2`, [req.brand.toLowerCase(), req.name.toLowerCase()]);
 }
 
-const deleteTTSData = (brand, name, ttsValue) => {
-    return pool.query('DELETE ttsData FROM ttsData WHERE brand = $1 AND name = $2 AND ttsData = $3', [brand.toLowerCase, name.toLowerCase, parseInt(ttsValue)]);
-}
-
 module.exports = {addData, getAverage}
